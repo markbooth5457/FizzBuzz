72aba6bfe0ee12a356347f565e3461123d486c9b
Mark Booth <mark@markandpol.com>
2019-11-17 10:55:14 +0000

    Initial Commit

---
+++ FizzBuzz WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  FizzBuzz WatchKit Extension
+//
+//  Created by Mark Booth on 17/11/2019.
+//  Copyright © 2019 Mark Booth. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
